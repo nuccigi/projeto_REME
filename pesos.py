ed59476e315131b88c557e9700e4046360c6a8ac
Giovanna Nucci <142818287+nuccigi@users.noreply.github.com>
2026-02-15 11:01:19 -0300

    Create pesos.py

---
+++ pesos.py
@@ -0,0 +1,15 @@
+# pesos.py
+
+pesos_ahp = {
+    "umidade": 250,
+    "eucalipto": 180,
+    "precipitacao": 140,
+    "temp_maxima": 90,
+    "area_umida": -80,
+    "represas_rios": -70,
+    "temp_media": 60,
+    "estrada": 50,
+    "eletrica": 50,
+    "moradores": 40,
+    "cerrado": 30
+}
